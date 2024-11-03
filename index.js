c05fffd7e47e5109d2be860b12549268af92d61c
vivekX86 <149016345+vivekX86@users.noreply.github.com>
2024-11-03 17:26:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.PORT;
+
+app.use(cors({
+    origin: "*",
+}))
 
 app.use(express.static("public"));
-
 const jsonData = {
     headline: "Shape the Future with KIIT E-Cell",
-    subText: "Join us at KIIT E-Cell and contribute to a culture of innovation and enterprenurship, there's a role for everyone",
+    subText: "Join us at KIIT E-Cell and contribute to a culture of innovation and enterprenurship, there's a role for everyone.",
     ctaText: "Start Your Tech Journey!",
     ctaLink: "https://forms.gle/foZQAvZEFiPtYpk79"
 }
